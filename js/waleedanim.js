a70d95c1d3044b9f0fc423ed905a33b70bed36e1
waliedAhmedSalah <127708352+waliedAhmedSalah@users.noreply.github.com>
2023-03-13 04:55:46 +0200

    Add files via upload

---
+++ js/waleedanim.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    'use strict';
+   // $("#ff").animate({height: '200px', opacity: '0.4'}, "slow");
+   // $("#ff").animate({left: '100px'}, "slow");
+    // var div = $("#ff");
+  //div.animate({left: '100px'}, "slow");
+  //div.animate({fontSize: '3em'}, "slow");
+     $("#ff").animate({
+    left: '50px',
+    opacity: '0.5'
+  });
+});
+
+
+ 
